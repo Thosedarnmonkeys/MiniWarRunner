9dadb35b46604b1113d78f2d49268400e71a7241
Thosedarnmonkeys <thosedarnmonkeys@googlemail.com>
2020-03-14 23:05:34 +0000

    Added SQLDAOService

---
+++ MiniWarRunner/Data/SQLDAOService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiniWarRunner.Data
+{
+  public class SQLDAOService
+  {
+    private string connectionString;
+
+    public SQLDAOService(string connectionString)
+    {
+      this.connectionString = connectionString;
+    }
+
+    public async Task<bool> TestConnect()
+    {
+      using (SqlConnection connection = new SqlConnection(connectionString))
+      {
+        try
+        {
+          await connection.OpenAsync();
+          return true;
+        }
+        catch (Exception)
+        {
+          return false;
+        }
+      }
+    }
+  }
+}
